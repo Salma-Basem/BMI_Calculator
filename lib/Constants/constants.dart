59379129a036fddb9be062d905654161cf5fb330
salma basem <20191743@fue.edu.eg>
2022-09-10 23:30:27 +0200

    comments

--- lib/Constants/constants.dart
+++ lib/Constants/constants.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 
+// grey shade
 final Color mainHexColor = HexColor("#1C1C1C");
+// light yellow
 final Color accentHexColor = HexColor("#FCC91C");
 
 
